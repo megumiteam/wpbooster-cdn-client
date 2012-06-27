b02cb5ff6d02f82f29167c34b9d01015c6189b3d
Takayuki Miyauchi <miya@theta.ne.jp>
2012-06-28 05:28:21 +0900

    language files added

--- readme.txt
+++ readme.txt
@@ -4,6 +4,8 @@ Tags: wpbooster, CDN, cloudfront
 Requires at least: 3.2
 Tested up to: 3.4
 Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 WPBooster CDN will make your WordPress sites load faster and perform Better!
 
--- wpbooster-cdn-client.php
+++ wpbooster-cdn-client.php
@@ -9,6 +9,27 @@ Domain Path: /languages
 Text Domain: wpbooster-cdn-client
 */
 
+/*
+Copyright (c) 2012 Takayuki Miyauchi (DigitalCube Co,.Ltd).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 new WPBoosterCDN();
 
@@ -86,7 +107,7 @@ public function admin_notice()
 {
     printf(
         '<div class="error"><p>%s</p></div>',
-        __('You can not activate WPBooster CDN. <a href="http://wpbooster.net/">Please Register to WPBooster Account.</a>')
+        __('You can not activate WPBooster CDN. <a href="http://wpbooster.net/">Please Register to WPBooster Account.</a>', 'wpbooster-cdn-client')
     );
 }
 
