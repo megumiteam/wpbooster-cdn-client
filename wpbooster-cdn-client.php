cfb6a5e48cba023b7d44622abd008d2a3e10c4ab
miya <miya@theta.ne.jp>
2012-09-10 14:09:46 +0900

    some fix

--- wpbooster-cdn-client.php
+++ wpbooster-cdn-client.php
@@ -3,7 +3,7 @@
 Plugin Name: The WP Booster CDN Client
 Author: Digitalcube Co,.Ltd (Takayuki Miyauchi)
 Description: Deliver static files from WPBooster CDN.
-Version: 2.0.0
+Version: 2.1.0
 Author URI: http://wpbooster.net/
 Domain Path: /languages
 Text Domain: wpbooster-cdn-client
